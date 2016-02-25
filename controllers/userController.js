44e8b3c195eed263186a82b795c36f5c9e25f610
aadams22 <ashleighadams@Ashleighs-MacBook-Air.local>
2016-02-24 20:51:18 -0500

    committing change from req.user.id to req.params.id  medicine update mongo set

--- controllers/userController.js
+++ controllers/userController.js
@@ -106,7 +106,7 @@ router.post('/:id', function(req,res){
 //EDIT MEDS
 router.put('/:id', function(req,res){
 // 	// Med.findByIdAndUpdate(req.body.id, req.body, function(err,data){
-	User.update({_id: req.user.id, 'meds._id': req.body.id}, 
+	User.update({_id: req.params.id, 'meds._id': req.body.id}, 
 		{$set:{'meds.$.dosage': req.body.dosage}},
 		{$set:{'meds.$.name': req.body.name}}, 
 		{$set:{'meds.$.pillNum': req.body.pillNum}}, 
